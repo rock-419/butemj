45518cefe681cb26716e5238b5dd30ebb23e40fa
rock <chuluudai.lkh@gmail.com>
2026-03-28 15:01:13 +0800

    1.01 finalized

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
+const elements = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+elements.forEach((el) => observer.observe(el));
